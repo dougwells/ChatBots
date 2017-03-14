b86c86576ad78b61dedb6dd34dd4fb02e4f6ebed
dougwells <dougwells@gmail.com>
2017-03-13 20:46:30 -0600

    ChatBots. MotionAI. Add "Guess my number" game.

--- MotionAI/randomNumber.js
+++ MotionAI/randomNumber.js
@@ -1,10 +1,31 @@
 'use strict';
 
+const getRandom = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
 module.exports = function(ctx, cb) {
   // ctx -> Context. Data sent TO this micro service
   // cb  -> Callback.  Sends data BACK to app calling this service
 
-  console.log(ctx);
-  return cb(null, {});
+  let getResult = getRandom(1,4);
+  console.log("Let", getResult)
+
+  if(ctx.data.replyData == getResult) {
+    console.log("true", getResult)
+    return cb(null, {
+      result: "Yeah!  You won!",
+      resultVal: String(getResult)
+    })
+
+  } else {
+    console.log("false", getResult)
+    return cb(null, {
+      result: "Sorry. You did not win this time...",
+      resultVal: String(getResult)
+    })
+  }
 
 }
